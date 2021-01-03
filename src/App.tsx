859b5b214f8876586c9be4ea7971a60a68aaceba
Gabriel <famosipe2010@gmail.com>
2021-01-03 14:32:54 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import axios from 'axios';
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
-function App() {
+export default function SimpleCard() {
+  const [loadData, setLoadData] = useState<any>([]);
+  console.log({Data: loadData});
+  useEffect(()=> {
+    axios.get(`https://softmesh-backend.herokuapp.com/usermsg`)
+      .then(function (response) {
+        // handle success
+        setLoadData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  },[]);
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {loadData.map((load :any, index: number) => (
+         <Card className={classes.root} style={{marginBottom: 15}} key={index}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {load.email}
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {load.fullName}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {load.phoneNumber}
+            </Typography>
+            <Typography variant="body2" component="p">
+              {load.msg}
+              <br />
+            </Typography>
+          </CardContent>
+          <CardActions>
+            {/* <Button size="small">Learn More</Button> */}
+          </CardActions>
+        </Card>
+      ))}
+    </Container>
   );
 }
-
-export default App;
